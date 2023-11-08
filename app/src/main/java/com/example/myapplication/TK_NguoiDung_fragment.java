a0f0434b60f310b936305a5dcf0d8bd8bff59936
Thanh VT <thanhvtps31375@fpt.edu.vn>
2023-11-08 08:49:21 +0700

    Thanh

--- app/src/main/java/com/example/myapplication/TK_NguoiDung_fragment.java
+++ app/src/main/java/com/example/myapplication/TK_NguoiDung_fragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class TK_NguoiDung_fragment extends Fragment {
 
+    private TextView txtDungMP,txtDungTP;
+    private CardView cvLichSuNghe,cvPlaylistYT,cvCaiDat,cvDangXuat;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,13 @@ public class TK_NguoiDung_fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.taikhoannguoidung_fragment,container,false);
+        txtDungMP = v.findViewById(R.id.txtDungMP);
+        txtDungTP = v.findViewById(R.id.txtDungTP);
+        cvLichSuNghe = v.findViewById(R.id.cvLichSuNghe);
+        cvPlaylistYT = v.findViewById(R.id.cvPlaylistYT);
+        cvCaiDat = v.findViewById(R.id.cvCaiDat);
+        cvDangXuat = v.findViewById(R.id.cvDangXuat);
+        return v;
     }
 }
