614e34ce40d0596969c909a4514286992449872c
duytammm <nguyenduytam20042004@gmail.com>
2023-11-08 08:59:57 +0700

    ditam

---
+++ app/src/main/java/com/example/myapplication/duytam/Chitietnhac.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.duytam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Chitietnhac extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chitietnhac);
+    }
+}
