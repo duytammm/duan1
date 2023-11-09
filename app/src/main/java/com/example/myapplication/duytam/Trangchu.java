24108916c5dc47eb81ffee1f9a0157916c3d1e56
duytammm <nguyenduytam20042004@gmail.com>
2023-11-09 11:23:59 +0700

    ditam

--- app/src/main/java/com/example/myapplication/duytam/Trangchu.java
+++ app/src/main/java/com/example/myapplication/duytam/Trangchu.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.duytam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -13,4 +14,7 @@ public class Trangchu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trangchu);
     }
+
+
+
 }
