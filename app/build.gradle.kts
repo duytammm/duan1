7c9e220291c07bbc2a81f91cfb32bb8bb7e9ae0d
Nhu Ngoc <ngoclnps31275@fpt.edu.vn>
2023-11-09 12:30:22 +0700

    Nhu Ngoc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     //Thư viện tạo slideshow
     implementation ("me.relex:circleindicator:2.1.6")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("me.relex:circleindicator:1.3.2")
 }
